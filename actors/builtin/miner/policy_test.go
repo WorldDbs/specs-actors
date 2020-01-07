f6336acba467d5b70a366e603f22f4729311abb6
boringland <boringland@protonmail.ch>
2020-01-07 16:37:18 +0000

    test - iPad cloud to TT to WC

--- actors/builtin/miner/policy_test.go
+++ actors/builtin/miner/policy_test.go
@@ -7,8 +7,8 @@ import (
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 )
 
 func TestQuality(t *testing.T) {
