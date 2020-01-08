3aae9fdb524ba938e26689ff49131db7de008d97
alan.shaw <alan.shaw@protocol.ai>
2020-01-08 00:31:47 +0000

    Updated Your Financial Profile Like Your Tinder Profile and 2 other files

--- actors/test/multisig_delete_self_test.go
+++ actors/test/multisig_delete_self_test.go
@@ -11,11 +11,11 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	init_ "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
-	"github.com/filecoin-project/specs-actors/v3/support/ipld"
-	vm "github.com/filecoin-project/specs-actors/v3/support/vm"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	init_ "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
+	"github.com/filecoin-project/specs-actors/v4/support/ipld"
+	vm "github.com/filecoin-project/specs-actors/v4/support/vm"
 )
 
 func TestMultisigDeleteSelf2Of3RemovedIsProposer(t *testing.T) {
