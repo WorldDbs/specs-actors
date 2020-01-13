2023c53fdb8b8a57bc46968abdea4c8847b1b255
alan.shaw <alan.shaw@protocol.ai>
2020-01-13 23:45:55 +0000

    Fix README titles

--- actors/util/adt/balancetable_test.go
+++ actors/util/adt/balancetable_test.go
@@ -9,10 +9,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v3/actors/util/adt"
-	"github.com/filecoin-project/specs-actors/v3/support/mock"
-	tutil "github.com/filecoin-project/specs-actors/v3/support/testing"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v4/support/mock"
+	tutil "github.com/filecoin-project/specs-actors/v4/support/testing"
 )
 
 func TestBalanceTable(t *testing.T) {
