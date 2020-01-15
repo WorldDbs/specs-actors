b3287117d10bec160aeac4f3e59dfbdf5cfd7990
fjl <fjl@ethereum.org>
2020-01-15 22:03:01 +0000

    b5c15fbc-2e6b-11e5-9284-b827eb9e62be

--- actors/builtin/miner/monies_test.go
+++ actors/builtin/miner/monies_test.go
@@ -7,9 +7,9 @@ import (
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
-	"github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
 )
 
 // Test termination fee
@@ -99,7 +99,7 @@ func TestPledgePenaltyForTermination(t *testing.T) {
 		dayReward := big.Div(initialPledge, bigInitialPledgeFactor)
 		twentyDayReward := big.Mul(dayReward, bigInitialPledgeFactor)
 		sectorAge := abi.ChainEpoch(20 * builtin.EpochsInDay)
-		replacementAge := abi.ChainEpoch(miner.TerminationLifetimeCap+ 1) * builtin.EpochsInDay
+		replacementAge := abi.ChainEpoch(miner.TerminationLifetimeCap+1) * builtin.EpochsInDay
 
 		// use low power, so we don't test SP=SP
 		power := big.NewInt(1)
@@ -153,4 +153,64 @@ func TestNegativeBRClamp(t *testing.T) {
 
 	fourBR := miner.ExpectedRewardForPower(rewardEstimate, powerEstimate, qaSectorPower, abi.ChainEpoch(4))
 	assert.Equal(t, big.Zero(), fourBR)
+}
+
+func TestContinuedFault(t *testing.T) {
+	t.Run("zero power means zero fault penalty", func(t *testing.T) {
+		epochTargetReward := abi.NewTokenAmount(1 << 50)
+		zeroQAPower := abi.NewStoragePower(0)
+		networkQAPower := abi.NewStoragePower(1 << 10)
+		powerRateOfChange := abi.NewStoragePower(1 << 10)
+		rewardEstimate := smoothing.NewEstimate(epochTargetReward, big.Zero())
+		powerEstimate := smoothing.NewEstimate(networkQAPower, powerRateOfChange)
+
+		penaltyForZeroPowerFaulted := miner.PledgePenaltyForContinuedFault(rewardEstimate, powerEstimate, zeroQAPower)
+		assert.Equal(t, big.Zero(), penaltyForZeroPowerFaulted)
+	})
+}
+
+func TestExpectedRewardForPowerClamptedAtAttoFIL(t *testing.T) {
+	t.Run("expected zero valued BR clamped at 1 attofil", func(t *testing.T) {
+		epochTargetReward := abi.NewTokenAmount(1 << 50)
+		zeroQAPower := abi.NewStoragePower(0)
+		networkQAPower := abi.NewStoragePower(1 << 10)
+		powerRateOfChange := abi.NewStoragePower(1 << 10)
+		rewardEstimate := smoothing.NewEstimate(epochTargetReward, big.Zero())
+		powerEstimate := smoothing.NewEstimate(networkQAPower, powerRateOfChange)
+
+		brClamped := miner.ExpectedRewardForPowerClampedAtAttoFIL(rewardEstimate, powerEstimate, zeroQAPower, abi.ChainEpoch(1))
+		assert.Equal(t, big.NewInt(1), brClamped)
+	})
+
+	t.Run("expected negative valued BR clamped at 1 atto FIL", func(t *testing.T) {
+		epochTargetReward := abi.NewTokenAmount(1 << 50)
+		qaSectorPower := abi.NewStoragePower(1 << 36)
+		networkQAPower := abi.NewStoragePower(1 << 10)
+		powerRateOfChange := abi.NewStoragePower(1 << 10).Neg()
+		rewardEstimate := smoothing.NewEstimate(epochTargetReward, big.Zero())
+		powerEstimate := smoothing.NewEstimate(networkQAPower, powerRateOfChange)
+
+		fourBRClamped := miner.ExpectedRewardForPowerClampedAtAttoFIL(rewardEstimate, powerEstimate, qaSectorPower, abi.ChainEpoch(4))
+		assert.Equal(t, big.NewInt(1), fourBRClamped)
+	})
+
+}
+
+func TestPrecommitDepositAndInitialPledgePostiive(t *testing.T) {
+	epochTargetReward := abi.NewTokenAmount(0) // zero reward so IP Base unclamped is 0
+	qaSectorPower := abi.NewStoragePower(1 << 36)
+	networkQAPower := abi.NewStoragePower(1 << 10)
+	baselinePower := networkQAPower
+	powerRateOfChange := abi.NewStoragePower(1 << 10)
+	rewardEstimate := smoothing.NewEstimate(epochTargetReward, big.Zero())
+	powerEstimate := smoothing.NewEstimate(networkQAPower, powerRateOfChange)
+	circulatingSupply := abi.NewTokenAmount(0)
+	t.Run("IP is clamped at 1 attofil", func(t *testing.T) {
+		ip := miner.InitialPledgeForPower(qaSectorPower, baselinePower, rewardEstimate, powerEstimate, circulatingSupply)
+		assert.Equal(t, abi.NewTokenAmount(1), ip)
+	})
+	t.Run("PCD is clamped at 1 attoFIL", func(t *testing.T) {
+		pcd := miner.PreCommitDepositForPower(rewardEstimate, powerEstimate, qaSectorPower)
+		assert.Equal(t, abi.NewTokenAmount(1), pcd)
+	})
 }
