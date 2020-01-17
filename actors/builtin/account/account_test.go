9c1e3a9bd698b1ef73ad6114eb4c9eb86656fd02
fjl <fjl@ethereum.org>
2020-01-17 22:14:09 +0000

    Little cleansing

--- actors/builtin/account/account_test.go
+++ actors/builtin/account/account_test.go
@@ -9,10 +9,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
-	"github.com/filecoin-project/specs-actors/v3/support/mock"
-	tutil "github.com/filecoin-project/specs-actors/v3/support/testing"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin/account"
+	"github.com/filecoin-project/specs-actors/v4/support/mock"
+	tutil "github.com/filecoin-project/specs-actors/v4/support/testing"
 )
 
 type constructorTestCase struct {
