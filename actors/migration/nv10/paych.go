a48dcbb412b5aef06d2795e4fb0c95e06569d57e
hugomrdias <hugomrdias@gmail.com>
2020-01-19 02:29:32 +0000

    Added couple dependent classes. Removed direct references to DC.

--- actors/migration/nv10/paych.go
+++ actors/migration/nv10/paych.go
@@ -7,8 +7,8 @@ import (
 	cid "github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
+	builtin3 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	paych3 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
 )
 
 type paychMigrator struct{}
