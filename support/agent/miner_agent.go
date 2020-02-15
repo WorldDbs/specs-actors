05d35c13e4fffa745b8fbfb7d2212c507227f143
davidad <davidad@alum.mit.edu>
2020-02-15 14:37:58 +0000

    add done toggle

--- support/agent/miner_agent.go
+++ support/agent/miner_agent.go
@@ -16,10 +16,10 @@ import (
 	mh "github.com/multiformats/go-multihash"
 	"github.com/pkg/errors"
 
-	"github.com/filecoin-project/specs-actors/v5/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v5/actors/builtin/market"
-	"github.com/filecoin-project/specs-actors/v5/actors/builtin/miner"
-	"github.com/filecoin-project/specs-actors/v5/actors/runtime/proof"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/v4/actors/runtime/proof"
 )
 
 type MinerAgentConfig struct {
