1b084001047726237e84a68d670339d772664eef
lexy8russo <lexy8russo@outlook.com>
2020-02-20 16:11:10 +0000

    Fix ChangeLog typos in previous commit.

--- actors/util/math/ln_test.go
+++ actors/util/math/ln_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/specs-actors/v5/actors/util/math"
+	"github.com/filecoin-project/specs-actors/v4/actors/util/math"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
