2451d9acb4e3836603db83122d65cc6c60db33c2
witek <witek@enjin.io>
2020-02-20 03:31:22 +0000

    Added include_path and autorun for test writer.

--- support/agent/miner_state.go
+++ support/agent/miner_state.go
@@ -7,21 +7,21 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/dline"
-	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
-	miner5 "github.com/filecoin-project/specs-actors/v5/actors/builtin/miner"
-	"github.com/filecoin-project/specs-actors/v5/actors/util/adt"
+	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
+	miner3 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 	cid "github.com/ipfs/go-cid"
 )
 
-type MinerStateV4 struct {
+type MinerStateV2 struct {
 	Root cid.Cid
 	Ctx  context.Context
-	st   *miner4.State
+	st   *miner2.State
 }
 
-func (m *MinerStateV4) state(store adt.Store) (*miner4.State, error) {
+func (m *MinerStateV2) state(store adt.Store) (*miner2.State, error) {
 	if m.st == nil {
-		var st miner4.State
+		var st miner2.State
 		err := store.Get(m.Ctx, m.Root, &st)
 		if err != nil {
 			return nil, err
@@ -31,7 +31,7 @@ func (m *MinerStateV4) state(store adt.Store) (*miner4.State, error) {
 	return m.st, nil
 }
 
-func (m *MinerStateV4) HasSectorNo(store adt.Store, sectorNo abi.SectorNumber) (bool, error) {
+func (m *MinerStateV2) HasSectorNo(store adt.Store, sectorNo abi.SectorNumber) (bool, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return false, err
@@ -39,7 +39,7 @@ func (m *MinerStateV4) HasSectorNo(store adt.Store, sectorNo abi.SectorNumber) (
 	return st.HasSectorNo(store, sectorNo)
 }
 
-func (m *MinerStateV4) FindSector(store adt.Store, sectorNo abi.SectorNumber) (uint64, uint64, error) {
+func (m *MinerStateV2) FindSector(store adt.Store, sectorNo abi.SectorNumber) (uint64, uint64, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return 0, 0, err
@@ -47,7 +47,7 @@ func (m *MinerStateV4) FindSector(store adt.Store, sectorNo abi.SectorNumber) (u
 	return st.FindSector(store, sectorNo)
 }
 
-func (m *MinerStateV4) ProvingPeriodStart(store adt.Store) (abi.ChainEpoch, error) {
+func (m *MinerStateV2) ProvingPeriodStart(store adt.Store) (abi.ChainEpoch, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return 0, err
@@ -55,7 +55,7 @@ func (m *MinerStateV4) ProvingPeriodStart(store adt.Store) (abi.ChainEpoch, erro
 	return st.ProvingPeriodStart, nil
 }
 
-func (m *MinerStateV4) LoadSectorInfo(store adt.Store, sectorNo uint64) (SimSectorInfo, error) {
+func (m *MinerStateV2) LoadSectorInfo(store adt.Store, sectorNo uint64) (SimSectorInfo, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return nil, err
@@ -64,10 +64,10 @@ func (m *MinerStateV4) LoadSectorInfo(store adt.Store, sectorNo uint64) (SimSect
 	if err != nil {
 		return nil, err
 	}
-	return &SectorInfoV4{info: sectors[0]}, nil
+	return &SectorInfoV2{info: sectors[0]}, nil
 }
 
-func (m *MinerStateV4) DeadlineInfo(store adt.Store, currEpoch abi.ChainEpoch) (*dline.Info, error) {
+func (m *MinerStateV2) DeadlineInfo(store adt.Store, currEpoch abi.ChainEpoch) (*dline.Info, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return nil, err
@@ -75,7 +75,7 @@ func (m *MinerStateV4) DeadlineInfo(store adt.Store, currEpoch abi.ChainEpoch) (
 	return st.DeadlineInfo(currEpoch), nil
 }
 
-func (m *MinerStateV4) FeeDebt(store adt.Store) (abi.TokenAmount, error) {
+func (m *MinerStateV2) FeeDebt(store adt.Store) (abi.TokenAmount, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return big.Zero(), err
@@ -83,7 +83,7 @@ func (m *MinerStateV4) FeeDebt(store adt.Store) (abi.TokenAmount, error) {
 	return st.FeeDebt, nil
 }
 
-func (m *MinerStateV4) LoadDeadlineState(store adt.Store, dlIdx uint64) (SimDeadlineState, error) {
+func (m *MinerStateV2) LoadDeadlineState(store adt.Store, dlIdx uint64) (SimDeadlineState, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return nil, err
@@ -96,46 +96,46 @@ func (m *MinerStateV4) LoadDeadlineState(store adt.Store, dlIdx uint64) (SimDead
 	if err != nil {
 		return nil, err
 	}
-	return &DeadlineStateV4{deadline: dline}, nil
+	return &DeadlineStateV2{deadline: dline}, nil
 }
 
-type DeadlineStateV4 struct {
-	deadline *miner4.Deadline
+type DeadlineStateV2 struct {
+	deadline *miner2.Deadline
 }
 
-func (d *DeadlineStateV4) LoadPartition(store adt.Store, partIdx uint64) (SimPartitionState, error) {
+func (d *DeadlineStateV2) LoadPartition(store adt.Store, partIdx uint64) (SimPartitionState, error) {
 	part, err := d.deadline.LoadPartition(store, partIdx)
 	if err != nil {
 		return nil, err
 	}
-	return &PartitionStateV4{partition: part}, nil
+	return &PartitionStateV2{partition: part}, nil
 }
 
-type PartitionStateV4 struct {
-	partition *miner4.Partition
+type PartitionStateV2 struct {
+	partition *miner2.Partition
 }
 
-func (p *PartitionStateV4) Terminated() bitfield.BitField {
+func (p *PartitionStateV2) Terminated() bitfield.BitField {
 	return p.partition.Terminated
 }
 
-type SectorInfoV4 struct {
-	info *miner4.SectorOnChainInfo
+type SectorInfoV2 struct {
+	info *miner2.SectorOnChainInfo
 }
 
-func (s *SectorInfoV4) Expiration() abi.ChainEpoch {
+func (s *SectorInfoV2) Expiration() abi.ChainEpoch {
 	return s.info.Expiration
 }
 
-type MinerStateV5 struct {
+type MinerStateV3 struct {
 	Root cid.Cid
-	st   *miner5.State
+	st   *miner3.State
 	Ctx  context.Context
 }
 
-func (m *MinerStateV5) state(store adt.Store) (*miner5.State, error) {
+func (m *MinerStateV3) state(store adt.Store) (*miner3.State, error) {
 	if m.st == nil {
-		var st miner5.State
+		var st miner3.State
 		err := store.Get(m.Ctx, m.Root, &st)
 		if err != nil {
 			return nil, err
@@ -145,7 +145,7 @@ func (m *MinerStateV5) state(store adt.Store) (*miner5.State, error) {
 	return m.st, nil
 }
 
-func (m *MinerStateV5) HasSectorNo(store adt.Store, sectorNo abi.SectorNumber) (bool, error) {
+func (m *MinerStateV3) HasSectorNo(store adt.Store, sectorNo abi.SectorNumber) (bool, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return false, err
@@ -153,7 +153,7 @@ func (m *MinerStateV5) HasSectorNo(store adt.Store, sectorNo abi.SectorNumber) (
 	return st.HasSectorNo(store, sectorNo)
 }
 
-func (m *MinerStateV5) FindSector(store adt.Store, sectorNo abi.SectorNumber) (uint64, uint64, error) {
+func (m *MinerStateV3) FindSector(store adt.Store, sectorNo abi.SectorNumber) (uint64, uint64, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return 0, 0, err
@@ -161,7 +161,7 @@ func (m *MinerStateV5) FindSector(store adt.Store, sectorNo abi.SectorNumber) (u
 	return st.FindSector(store, sectorNo)
 }
 
-func (m *MinerStateV5) ProvingPeriodStart(store adt.Store) (abi.ChainEpoch, error) {
+func (m *MinerStateV3) ProvingPeriodStart(store adt.Store) (abi.ChainEpoch, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return 0, err
@@ -169,7 +169,7 @@ func (m *MinerStateV5) ProvingPeriodStart(store adt.Store) (abi.ChainEpoch, erro
 	return st.ProvingPeriodStart, nil
 }
 
-func (m *MinerStateV5) LoadSectorInfo(store adt.Store, sectorNo uint64) (SimSectorInfo, error) {
+func (m *MinerStateV3) LoadSectorInfo(store adt.Store, sectorNo uint64) (SimSectorInfo, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return nil, err
@@ -178,10 +178,10 @@ func (m *MinerStateV5) LoadSectorInfo(store adt.Store, sectorNo uint64) (SimSect
 	if err != nil {
 		return nil, err
 	}
-	return &SectorInfoV5{info: sectors[0]}, nil
+	return &SectorInfoV3{info: sectors[0]}, nil
 }
 
-func (m *MinerStateV5) DeadlineInfo(store adt.Store, currEpoch abi.ChainEpoch) (*dline.Info, error) {
+func (m *MinerStateV3) DeadlineInfo(store adt.Store, currEpoch abi.ChainEpoch) (*dline.Info, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return nil, err
@@ -189,7 +189,7 @@ func (m *MinerStateV5) DeadlineInfo(store adt.Store, currEpoch abi.ChainEpoch) (
 	return st.DeadlineInfo(currEpoch), nil
 }
 
-func (m *MinerStateV5) FeeDebt(store adt.Store) (abi.TokenAmount, error) {
+func (m *MinerStateV3) FeeDebt(store adt.Store) (abi.TokenAmount, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return big.Zero(), err
@@ -197,7 +197,7 @@ func (m *MinerStateV5) FeeDebt(store adt.Store) (abi.TokenAmount, error) {
 	return st.FeeDebt, nil
 }
 
-func (m *MinerStateV5) LoadDeadlineState(store adt.Store, dlIdx uint64) (SimDeadlineState, error) {
+func (m *MinerStateV3) LoadDeadlineState(store adt.Store, dlIdx uint64) (SimDeadlineState, error) {
 	st, err := m.state(store)
 	if err != nil {
 		return nil, err
@@ -210,33 +210,33 @@ func (m *MinerStateV5) LoadDeadlineState(store adt.Store, dlIdx uint64) (SimDead
 	if err != nil {
 		return nil, err
 	}
-	return &DeadlineStateV5{deadline: dline}, nil
+	return &DeadlineStateV3{deadline: dline}, nil
 }
 
-type DeadlineStateV5 struct {
-	deadline *miner5.Deadline
+type DeadlineStateV3 struct {
+	deadline *miner3.Deadline
 }
 
-func (d *DeadlineStateV5) LoadPartition(store adt.Store, partIdx uint64) (SimPartitionState, error) {
+func (d *DeadlineStateV3) LoadPartition(store adt.Store, partIdx uint64) (SimPartitionState, error) {
 	part, err := d.deadline.LoadPartition(store, partIdx)
 	if err != nil {
 		return nil, err
 	}
-	return &PartitionStateV5{partition: part}, nil
+	return &PartitionStateV3{partition: part}, nil
 }
 
-type PartitionStateV5 struct {
-	partition *miner5.Partition
+type PartitionStateV3 struct {
+	partition *miner3.Partition
 }
 
-func (p *PartitionStateV5) Terminated() bitfield.BitField {
+func (p *PartitionStateV3) Terminated() bitfield.BitField {
 	return p.partition.Terminated
 }
 
-type SectorInfoV5 struct {
-	info *miner5.SectorOnChainInfo
+type SectorInfoV3 struct {
+	info *miner3.SectorOnChainInfo
 }
 
-func (s *SectorInfoV5) Expiration() abi.ChainEpoch {
+func (s *SectorInfoV3) Expiration() abi.ChainEpoch {
 	return s.info.Expiration
 }
