f05db1253163797d21f06823b48b6407c60ccbda
davidad <davidad@alum.mit.edu>
2020-02-20 00:40:40 +0000

    Merge "Undercloud ctplane router for IPv6 RA's idempotent"

--- actors/builtin/paych/paych_test.go
+++ actors/builtin/paych/paych_test.go
@@ -17,11 +17,11 @@ import (
 	"github.com/stretchr/testify/require"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/specs-actors/v5/actors/builtin"
-	. "github.com/filecoin-project/specs-actors/v5/actors/builtin/paych"
-	"github.com/filecoin-project/specs-actors/v5/actors/util/adt"
-	"github.com/filecoin-project/specs-actors/v5/support/mock"
-	tutil "github.com/filecoin-project/specs-actors/v5/support/testing"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	. "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
+	"github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v4/support/mock"
+	tutil "github.com/filecoin-project/specs-actors/v4/support/testing"
 )
 
 func TestExports(t *testing.T) {
