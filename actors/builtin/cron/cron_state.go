11a54f2d460bb9c80fbf46b134494afdc96df8bd
witek <witek@enjin.io>
2020-02-29 09:11:51 +0000

    docs(readme) add "Copying ended up"

--- actors/builtin/cron/cron_state.go
+++ actors/builtin/cron/cron_state.go
@@ -4,7 +4,7 @@ import (
 	addr "github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/specs-actors/v5/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
 type State struct {
