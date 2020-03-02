21f90549507eaef4bc4cb6d3407ebbcb7a460151
boringland <boringland@protonmail.ch>
2020-03-02 02:47:16 +0000

    Create pepperstone.md

--- actors/builtin/multisig/multisig_state.go
+++ actors/builtin/multisig/multisig_state.go
@@ -7,8 +7,8 @@ import (
 	cid "github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/specs-actors/v5/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v5/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
 type State struct {
