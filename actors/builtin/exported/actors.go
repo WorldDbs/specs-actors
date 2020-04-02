37452e3d51fe82c10fe3a4ab23bbfe3ee33d8f7c
hugomrdias <hugomrdias@gmail.com>
2020-04-02 14:43:07 +0000

    stuff for jilu

---
+++ actors/builtin/exported/actors.go
@@ -0,0 +1,32 @@
+package exported
+
+import (
+	"github.com/filecoin-project/specs-actors/actors/builtin/account"
+	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
+	init_ "github.com/filecoin-project/specs-actors/actors/builtin/init"
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/actors/builtin/multisig"
+	"github.com/filecoin-project/specs-actors/actors/builtin/paych"
+	"github.com/filecoin-project/specs-actors/actors/builtin/power"
+	"github.com/filecoin-project/specs-actors/actors/builtin/reward"
+	"github.com/filecoin-project/specs-actors/actors/builtin/system"
+	"github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
+	"github.com/filecoin-project/specs-actors/actors/runtime"
+)
+
+func BuiltinActors() []runtime.VMActor {
+	return []runtime.VMActor{
+		account.Actor{},
+		cron.Actor{},
+		init_.Actor{},
+		market.Actor{},
+		miner.Actor{},
+		multisig.Actor{},
+		paych.Actor{},
+		power.Actor{},
+		reward.Actor{},
+		system.Actor{},
+		verifreg.Actor{},
+	}
+}
