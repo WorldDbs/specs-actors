4a578beb4a38dbc55dcef23636b0d47898318bdb
arachnid <arachnid@notdot.net>
2020-04-02 23:55:54 +0000

    readded freenode irc client

---
+++ specs-actors/v3@v3.1.0/actors/util/adt/array_test.go
@@ -0,0 +1,22 @@
+package adt_test
+
+import (
+	"testing"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/stretchr/testify/require"
+
+	"github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v3/support/mock"
+)
+
+func TestArrayNotFound(t *testing.T) {
+	rt := mock.NewBuilder(address.Undef).Build(t)
+	store := adt.AsStore(rt)
+	arr, err := adt.MakeEmptyArray(store, 3)
+	require.NoError(t, err)
+
+	found, err := arr.Get(7, nil)
+	require.NoError(t, err)
+	require.False(t, found)
+}
