b3db1e9fdd1df0665ba29e1091068e849eb4f66c
fjl <fjl@ethereum.org>
2020-04-04 06:11:12 +0000

    Update django-extensions from 2.1.3 to 2.1.5

---
+++ actors/builtin/sector.go
@@ -0,0 +1,111 @@
+package builtin
+
+import (
+	stabi "github.com/filecoin-project/go-state-types/abi"
+	"github.com/pkg/errors"
+)
+
+// Policy values associated with a seal proof type.
+type SealProofPolicy struct {
+	SectorMaxLifetime          stabi.ChainEpoch
+	ConsensusMinerMinPower     stabi.StoragePower
+}
+
+// For all Stacked DRG sectors, the max is 5 years
+const epochsPerYear = 1_051_200
+const fiveYears = stabi.ChainEpoch(5 * epochsPerYear)
+
+var SealProofPolicies = map[stabi.RegisteredSealProof]*SealProofPolicy{
+	stabi.RegisteredSealProof_StackedDrg2KiBV1: {
+		SectorMaxLifetime:          fiveYears,
+		ConsensusMinerMinPower:     stabi.NewStoragePower(0),
+	},
+	stabi.RegisteredSealProof_StackedDrg8MiBV1: {
+		SectorMaxLifetime:          fiveYears,
+		ConsensusMinerMinPower:     stabi.NewStoragePower(16 << 20),
+	},
+	stabi.RegisteredSealProof_StackedDrg512MiBV1: {
+		SectorMaxLifetime:          fiveYears,
+		ConsensusMinerMinPower:     stabi.NewStoragePower(1 << 30),
+	},
+	stabi.RegisteredSealProof_StackedDrg32GiBV1: {
+		SectorMaxLifetime:          fiveYears,
+		ConsensusMinerMinPower:     stabi.NewStoragePower(100 << 40),
+	},
+	stabi.RegisteredSealProof_StackedDrg64GiBV1: {
+		SectorMaxLifetime:          fiveYears,
+		ConsensusMinerMinPower:     stabi.NewStoragePower(200 << 40),
+	},
+}
+
+// Returns the partition size, in sectors, associated with a seal proof type.
+// The partition size is the number of sectors proved in a single PoSt proof.
+func SealProofWindowPoStPartitionSectors(p stabi.RegisteredSealProof) (uint64, error) {
+	wPoStProofType, err := p.RegisteredWindowPoStProof()
+	if err != nil {
+		return 0, err
+	}
+	return PoStProofWindowPoStPartitionSectors(wPoStProofType)
+}
+
+// SectorMaximumLifetime is the maximum duration a sector sealed with this proof may exist between activation and expiration
+func SealProofSectorMaximumLifetime(p stabi.RegisteredSealProof) (stabi.ChainEpoch, error) {
+	info, ok := SealProofPolicies[p]
+	if !ok {
+		return 0, errors.Errorf("unsupported proof type: %v", p)
+	}
+	return info.SectorMaxLifetime, nil
+}
+
+// The minimum power of an individual miner to meet the threshold for leader election (in bytes).
+// Motivation:
+// - Limits sybil generation
+// - Improves consensus fault detection
+// - Guarantees a minimum fee for consensus faults
+// - Ensures that a specific soundness for the power table
+// Note: We may be able to reduce this in the future, addressing consensus faults with more complicated penalties,
+// sybil generation with crypto-economic mechanism, and PoSt soundness by increasing the challenges for small miners.
+func ConsensusMinerMinPower(p stabi.RegisteredSealProof) (stabi.StoragePower, error) {
+	info, ok := SealProofPolicies[p]
+	if !ok {
+		return stabi.NewStoragePower(0), errors.Errorf("unsupported proof type: %v", p)
+	}
+	return info.ConsensusMinerMinPower, nil
+}
+
+// Policy values associated with a PoSt proof type.
+type PoStProofPolicy struct {
+	WindowPoStPartitionSectors uint64
+}
+
+// Partition sizes must match those used by the proofs library.
+// See https://github.com/filecoin-project/rust-fil-proofs/blob/master/filecoin-proofs/src/constants.rs#L85
+var PoStProofPolicies = map[stabi.RegisteredPoStProof]*PoStProofPolicy{
+	stabi.RegisteredPoStProof_StackedDrgWindow2KiBV1: {
+		WindowPoStPartitionSectors: 2,
+	},
+	stabi.RegisteredPoStProof_StackedDrgWindow8MiBV1: {
+		WindowPoStPartitionSectors: 2,
+	},
+	stabi.RegisteredPoStProof_StackedDrgWindow512MiBV1: {
+		WindowPoStPartitionSectors: 2,
+	},
+	stabi.RegisteredPoStProof_StackedDrgWindow32GiBV1: {
+		WindowPoStPartitionSectors: 2349,
+	},
+	stabi.RegisteredPoStProof_StackedDrgWindow64GiBV1: {
+		WindowPoStPartitionSectors: 2300,
+	},
+	// Winning PoSt proof types omitted.
+}
+
+
+// Returns the partition size, in sectors, associated with a Window PoSt proof type.
+// The partition size is the number of sectors proved in a single PoSt proof.
+func PoStProofWindowPoStPartitionSectors(p stabi.RegisteredPoStProof) (uint64, error) {
+	info, ok := PoStProofPolicies[p]
+	if !ok {
+		return 0, errors.Errorf("unsupported proof type: %v", p)
+	}
+	return info.WindowPoStPartitionSectors, nil
+}
---
+++ actors/util/bitfield_test.go
@@ -0,0 +1,96 @@
+package util_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/filecoin-project/go-bitfield"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/filecoin-project/specs-actors/actors/util"
+)
+
+func TestBitFieldUnset(t *testing.T) {
+	bf := bitfield.New()
+	bf.Set(1)
+	bf.Set(2)
+	bf.Set(3)
+	bf.Set(4)
+	bf.Set(5)
+
+	bf.Unset(3)
+
+	m, err := bf.AllMap(100)
+	assert.NoError(t, err)
+	_, found := m[3]
+	assert.False(t, found)
+
+	cnt, err := bf.Count()
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(4), cnt)
+
+	bf2 := roundtripMarshal(t, bf)
+
+	cnt, err = bf2.Count()
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(4), cnt)
+
+	m, err = bf.AllMap(100)
+	assert.NoError(t, err)
+	_, found = m[3]
+	assert.False(t, found)
+}
+
+func roundtripMarshal(t *testing.T, in bitfield.BitField) bitfield.BitField {
+	buf := new(bytes.Buffer)
+	err := in.MarshalCBOR(buf)
+	assert.NoError(t, err)
+
+	bf2 := bitfield.New()
+	err = bf2.UnmarshalCBOR(buf)
+	assert.NoError(t, err)
+	return bf2
+}
+
+func TestBitFieldContains(t *testing.T) {
+	a := bitfield.New()
+	a.Set(2)
+	a.Set(4)
+	a.Set(5)
+
+	b := bitfield.New()
+	b.Set(3)
+	b.Set(4)
+
+	c := bitfield.New()
+	c.Set(2)
+	c.Set(5)
+
+	assertContainsAny := func(a, b bitfield.BitField, expected bool) {
+		t.Helper()
+		actual, err := util.BitFieldContainsAny(a, b)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, actual)
+	}
+
+	assertContainsAll := func(a, b bitfield.BitField, expected bool) {
+		t.Helper()
+		actual, err := util.BitFieldContainsAll(a, b)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, actual)
+	}
+
+	assertContainsAny(a, b, true)
+	assertContainsAny(b, a, true)
+	assertContainsAny(a, c, true)
+	assertContainsAny(c, a, true)
+	assertContainsAny(b, c, false)
+	assertContainsAny(c, b, false)
+
+	assertContainsAll(a, b, false)
+	assertContainsAll(b, a, false)
+	assertContainsAll(a, c, true)
+	assertContainsAll(c, a, false)
+	assertContainsAll(b, c, false)
+	assertContainsAll(c, b, false)
+}
