18514d68112069346a66f11084b8feb8102969d6
mail <mail@bitpshr.net>
2020-04-09 05:25:59 +0000

    missed a bracket

---
+++ specs-actors/v3@v3.1.0/actors/builtin/system/system_actor.go
@@ -0,0 +1,41 @@
+package system
+
+import (
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/ipfs/go-cid"
+
+	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v3/actors/runtime"
+)
+
+type Actor struct{}
+
+func (a Actor) Exports() []interface{} {
+	return []interface{}{
+		builtin.MethodConstructor: a.Constructor,
+	}
+}
+
+func (a Actor) Code() cid.Cid {
+	return builtin.SystemActorCodeID
+}
+
+func (a Actor) IsSingleton() bool {
+	return true
+}
+
+func (a Actor) State() cbor.Er {
+	return new(State)
+}
+
+var _ runtime.VMActor = Actor{}
+
+func (a Actor) Constructor(rt runtime.Runtime, _ *abi.EmptyValue) *abi.EmptyValue {
+	rt.ValidateImmediateCallerIs(builtin.SystemActorAddr)
+
+	rt.StateCreate(&State{})
+	return nil
+}
+
+type State struct{}
