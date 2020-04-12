7fd990f38b3e9c0652a6d3ce3c02ec49a595c7ba
boringland <boringland@protonmail.ch>
2020-04-12 07:57:46 +0000

    Fix typo in offline.sgml

--- actors/builtin/miner/bitfield_queue_test.go
+++ actors/builtin/miner/bitfield_queue_test.go
@@ -10,9 +10,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin/miner"
-	"github.com/filecoin-project/specs-actors/actors/util/adt"
-	"github.com/filecoin-project/specs-actors/support/mock"
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v2/support/mock"
 )
 
 func TestBitfieldQueue(t *testing.T) {
