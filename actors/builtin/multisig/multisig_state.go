860d3d7eeb97165018fd3dab662d3a9e3e1e5819
nagydani <nagydani@epointsystem.org>
2020-04-13 19:59:26 +0000

    be36b7ca-2d3d-11e5-ab42-c82a142b6f9b

--- actors/builtin/multisig/multisig_state.go
+++ actors/builtin/multisig/multisig_state.go
@@ -2,13 +2,13 @@ package multisig
 
 import (
 	address "github.com/filecoin-project/go-address"
-	abi "github.com/filecoin-project/go-state-types/abi"
-	big "github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/exitcode"
 	cid "github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
-	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
 type State struct {
@@ -25,7 +25,7 @@ type State struct {
 	StartEpoch     abi.ChainEpoch
 	UnlockDuration abi.ChainEpoch
 
-	PendingTxns cid.Cid
+	PendingTxns cid.Cid // HAMT[TxnID]Transaction
 }
 
 func (st *State) SetLocked(startEpoch abi.ChainEpoch, unlockDuration abi.ChainEpoch, lockedAmount abi.TokenAmount) {
@@ -38,13 +38,80 @@ func (st *State) AmountLocked(elapsedEpoch abi.ChainEpoch) abi.TokenAmount {
 	if elapsedEpoch >= st.UnlockDuration {
 		return abi.NewTokenAmount(0)
 	}
-	if elapsedEpoch < 0 {
+	if elapsedEpoch <= 0 {
 		return st.InitialBalance
 	}
 
-	// TODO: fix division truncation https://github.com/filecoin-project/specs-actors/issues/1131
-	unitLocked := big.Div(st.InitialBalance, big.NewInt(int64(st.UnlockDuration)))
-	return big.Mul(unitLocked, big.Sub(big.NewInt(int64(st.UnlockDuration)), big.NewInt(int64(elapsedEpoch))))
+	unlockDuration := big.NewInt(int64(st.UnlockDuration))
+	remainingLockDuration := big.Sub(unlockDuration, big.NewInt(int64(elapsedEpoch)))
+
+	// locked = ceil(InitialBalance * remainingLockDuration / UnlockDuration)
+	numerator := big.Mul(st.InitialBalance, remainingLockDuration)
+	denominator := unlockDuration
+	quot := big.Div(numerator, denominator)
+	rem := big.Mod(numerator, denominator)
+
+	locked := quot
+	if !rem.IsZero() {
+		locked = big.Add(locked, big.NewInt(1))
+	}
+	return locked
+}
+
+// Iterates all pending transactions and removes an address from each list of approvals, if present.
+// If an approval list becomes empty, the pending transaction is deleted.
+func (st *State) PurgeApprovals(store adt.Store, addr address.Address) error {
+	txns, err := adt.AsMap(store, st.PendingTxns)
+	if err != nil {
+		return xerrors.Errorf("failed to load transactions: %w", err)
+	}
+
+	// Identify the transactions that need updating.
+	var txnIdsToPurge []string              // For stable iteration
+	txnsToPurge := map[string]Transaction{} // Values are not pointers, we need copies
+	var txn Transaction
+	if err = txns.ForEach(&txn, func(txid string) error {
+		for _, approver := range txn.Approved {
+			if approver == addr {
+				txnIdsToPurge = append(txnIdsToPurge, txid)
+				txnsToPurge[txid] = txn
+				break
+			}
+		}
+		return nil
+	}); err != nil {
+		return xerrors.Errorf("failed to traverse transactions: %w", err)
+	}
+
+	// Update or remove those transactions.
+	for _, txid := range txnIdsToPurge {
+		txn := txnsToPurge[txid]
+		// The right length is almost certainly len-1, but let's not be too clever.
+		newApprovers := make([]address.Address, 0, len(txn.Approved))
+		for _, approver := range txn.Approved {
+			if approver != addr {
+				newApprovers = append(newApprovers, approver)
+			}
+		}
+
+		if len(newApprovers) > 0 {
+			txn.Approved = newApprovers
+			if err := txns.Put(StringKey(txid), &txn); err != nil {
+				return xerrors.Errorf("failed to update transaction approvers: %w", err)
+			}
+		} else {
+			if err := txns.Delete(StringKey(txid)); err != nil {
+				return xerrors.Errorf("failed to delete transaction with no approvers: %w", err)
+			}
+		}
+	}
+
+	if newTxns, err := txns.Root(); err != nil {
+		return xerrors.Errorf("failed to persist transactions: %w", err)
+	} else {
+		st.PendingTxns = newTxns
+	}
+	return nil
 }
 
 // return nil if MultiSig maintains required locked balance after spending the amount, else return an error.
@@ -54,6 +121,10 @@ func (st *State) assertAvailable(currBalance abi.TokenAmount, amountToSpend abi.
 	}
 	if currBalance.LessThan(amountToSpend) {
 		return xerrors.Errorf("current balance %s less than amount to spend %s", currBalance.String(), amountToSpend.String())
+	}
+	if amountToSpend.IsZero() {
+		// Always permit a transaction that sends no value, even if the lockup exceeds the current balance.
+		return nil
 	}
 
 	remainingBalance := big.Sub(currBalance, amountToSpend)
@@ -76,4 +147,11 @@ func getPendingTransaction(ptx *adt.Map, txnID TxnID) (Transaction, error) {
 		return Transaction{}, exitcode.ErrNotFound.Wrapf("failed to find transaction %v", txnID)
 	}
 	return out, nil
+}
+
+// An adt.Map key that just preserves the underlying string.
+type StringKey string
+
+func (k StringKey) Key() string {
+	return string(k)
 }
