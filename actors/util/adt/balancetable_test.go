b773a0962cf33187bc965f4f5c2f490c67a8abbf
ng8eke <ng8eke@163.com>
2020-04-16 23:57:11 +0000

    Merge "making PKI default token type"

--- actors/util/adt/balancetable_test.go
+++ actors/util/adt/balancetable_test.go
@@ -10,9 +10,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/actors/util/adt"
-	"github.com/filecoin-project/specs-actors/support/mock"
-	tutil "github.com/filecoin-project/specs-actors/support/testing"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v2/support/mock"
+	tutil "github.com/filecoin-project/specs-actors/v2/support/testing"
 )
 
 func TestBalanceTable(t *testing.T) {
