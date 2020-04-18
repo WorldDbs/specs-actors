11dd0c05b27681753725aebd2c32d101c1571b16
alan.shaw <alan.shaw@protocol.ai>
2020-04-18 19:07:27 +0000

    Update temp-present-females.rq

--- actors/builtin/reward/reward_logic_test.go
+++ actors/builtin/reward/reward_logic_test.go
@@ -6,35 +6,35 @@ import (
 	gbig "math/big"
 	"testing"
 
-	. "github.com/filecoin-project/go-state-types/abi"
-	big "github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/go-state-types/network"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/stretchr/testify/assert"
 	"github.com/xorcare/golden"
 
-	"github.com/filecoin-project/specs-actors/actors/util/math"
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/math"
 )
 
 func q128ToF(x big.Int) float64 {
 	q128 := new(gbig.Int).SetInt64(1)
-	q128 = q128.Lsh(q128, math.Precision)
+	q128 = q128.Lsh(q128, math.Precision128)
 	res, _ := new(gbig.Rat).SetFrac(x.Int, q128).Float64()
 	return res
 }
 
 func TestComputeRTeta(t *testing.T) {
-	baselinePowerAt := func(epoch ChainEpoch) StoragePower {
+	baselinePowerAt := func(epoch abi.ChainEpoch) abi.StoragePower {
 		return big.Mul(big.NewInt(int64(epoch+1)), big.NewInt(2048))
 	}
 
-	assert.Equal(t, 0.5, q128ToF(computeRTheta(1, baselinePowerAt(1), big.NewInt(2048+2*2048*0.5), big.NewInt(2048+2*2048))))
-	assert.Equal(t, 0.25, q128ToF(computeRTheta(1, baselinePowerAt(1), big.NewInt(2048+2*2048*0.25), big.NewInt(2048+2*2048))))
+	assert.Equal(t, 0.5, q128ToF(ComputeRTheta(1, baselinePowerAt(1), big.NewInt(2048+2*2048*0.5), big.NewInt(2048+2*2048))))
+	assert.Equal(t, 0.25, q128ToF(ComputeRTheta(1, baselinePowerAt(1), big.NewInt(2048+2*2048*0.25), big.NewInt(2048+2*2048))))
 
 	cumsum15 := big.NewInt(0)
-	for i := ChainEpoch(0); i < 16; i++ {
+	for i := abi.ChainEpoch(0); i < 16; i++ {
 		cumsum15 = big.Add(cumsum15, baselinePowerAt(i))
 	}
-	assert.Equal(t, 15.25, q128ToF(computeRTheta(16,
+	assert.Equal(t, 15.25, q128ToF(ComputeRTheta(16,
 		baselinePowerAt(16),
 		big.Add(cumsum15, big.Div(baselinePowerAt(16), big.NewInt(4))),
 		big.Add(cumsum15, baselinePowerAt(16)))))
@@ -42,11 +42,11 @@ func TestComputeRTeta(t *testing.T) {
 
 func TestBaselineReward(t *testing.T) {
 	step := gbig.NewInt(5000)
-	step = step.Lsh(step, math.Precision)
+	step = step.Lsh(step, math.Precision128)
 	step = step.Sub(step, gbig.NewInt(77777777777)) // offset from full integers
 
 	delta := gbig.NewInt(1)
-	delta = delta.Lsh(delta, math.Precision)
+	delta = delta.Lsh(delta, math.Precision128)
 	delta = delta.Sub(delta, gbig.NewInt(33333333333)) // offset from full integers
 
 	prevTheta := new(gbig.Int)
@@ -54,10 +54,10 @@ func TestBaselineReward(t *testing.T) {
 
 	b := &bytes.Buffer{}
 	b.WriteString("t0, t1, y\n")
-	simple := computeReward(0, big.Zero(), big.Zero())
+	simple := computeReward(0, big.Zero(), big.Zero(), DefaultSimpleTotal, DefaultBaselineTotal)
 
 	for i := 0; i < 512; i++ {
-		reward := computeReward(0, big.Int{Int: prevTheta}, big.Int{Int: theta})
+		reward := computeReward(0, big.NewFromGo(prevTheta), big.NewFromGo(theta), DefaultSimpleTotal, DefaultBaselineTotal)
 		reward = big.Sub(reward, simple)
 		fmt.Fprintf(b, "%s,%s,%s\n", prevTheta, theta, reward.Int)
 		prevTheta = prevTheta.Add(prevTheta, step)
@@ -72,7 +72,7 @@ func TestSimpleReward(t *testing.T) {
 	b.WriteString("x, y\n")
 	for i := int64(0); i < 512; i++ {
 		x := i * 5000
-		reward := computeReward(ChainEpoch(x), big.Zero(), big.Zero())
+		reward := computeReward(abi.ChainEpoch(x), big.Zero(), big.Zero(), DefaultSimpleTotal, DefaultBaselineTotal)
 		fmt.Fprintf(b, "%d,%s\n", x, reward.Int)
 	}
 
@@ -81,50 +81,50 @@ func TestSimpleReward(t *testing.T) {
 
 func TestBaselineRewardGrowth(t *testing.T) {
 
-	baselineInYears := func(start StoragePower, x ChainEpoch) StoragePower {
+	baselineInYears := func(start abi.StoragePower, x abi.ChainEpoch) abi.StoragePower {
 		baseline := start
-		for i := ChainEpoch(0); i < x*epochsInYear; i++ {
-			baseline = BaselinePowerFromPrev(baseline, network.Version0)
+		for i := abi.ChainEpoch(0); i < x*builtin.EpochsInYear; i++ {
+			baseline = BaselinePowerFromPrev(baseline)
 		}
 		return baseline
 	}
 
-	// Baseline reward should have 200% growth rate
+	// Baseline reward should have 100% growth rate
 	// This implies that for every year x, the baseline function should be:
-	// StartVal * 3^x.
+	// StartVal * 2^x.
 	//
 	// Error values for 1 years of growth were determined empirically with latest
 	// baseline power construction to set bounds in this test in order to
 	// 1. throw a test error if function changes and percent error goes up
 	// 2. serve as documentation of current error bounds
 	type growthTestCase struct {
-		StartVal StoragePower
+		StartVal abi.StoragePower
 		ErrBound float64
 	}
 	cases := []growthTestCase{
 		// 1 byte
 		{
-			NewStoragePower(1),
+			abi.NewStoragePower(1),
 			1,
 		},
 		// GiB
 		{
-			NewStoragePower(1 << 30),
+			abi.NewStoragePower(1 << 30),
 			1e-3,
 		},
 		// TiB
 		{
-			NewStoragePower(1 << 40),
+			abi.NewStoragePower(1 << 40),
 			1e-6,
 		},
 		// PiB
 		{
-			NewStoragePower(1 << 50),
+			abi.NewStoragePower(1 << 50),
 			1e-8,
 		},
 		// EiB
 		{
-			BaselineInitialValueV0,
+			BaselineInitialValue,
 			1e-8,
 		},
 		// ZiB
@@ -134,15 +134,15 @@ func TestBaselineRewardGrowth(t *testing.T) {
 		},
 		// non power of 2 ~ 1 EiB
 		{
-			NewStoragePower(513633559722596517),
+			abi.NewStoragePower(513633559722596517),
 			1e-8,
 		},
 	}
 	for _, testCase := range cases {
 		years := int64(1)
-		end := baselineInYears(testCase.StartVal, ChainEpoch(1))
+		end := baselineInYears(testCase.StartVal, abi.ChainEpoch(1))
 
-		multiplier := big.Exp(big.NewInt(3), big.NewInt(years)) // keeping this generalized in case we want to test more years
+		multiplier := big.Exp(big.NewInt(2), big.NewInt(years)) // keeping this generalized in case we want to test more years
 		expected := big.Mul(testCase.StartVal, multiplier)
 		diff := big.Sub(expected, end)
 
