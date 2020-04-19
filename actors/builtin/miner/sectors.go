ad9c10a432d5264427566d3e88a0f26f8a00ff07
witek <witek@enjin.io>
2020-04-19 22:02:14 +0000

    override save() and delete() functions to use helper class AdhUser

--- actors/builtin/miner/sectors.go
+++ actors/builtin/miner/sectors.go
@@ -9,7 +9,7 @@ import (
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/specs-actors/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
 func LoadSectors(store adt.Store, root cid.Cid) (Sectors, error) {
@@ -69,11 +69,6 @@ func (sa Sectors) Store(infos ...*SectorOnChainInfo) error {
 			return fmt.Errorf("failed to store sector %d: %w", info.SectorNumber, err)
 		}
 	}
-
-	if sa.Length() > SectorsMax {
-		return xerrors.Errorf("too many sectors")
-	}
-
 	return nil
 }
 
@@ -85,4 +80,90 @@ func (sa Sectors) MustGet(sectorNumber abi.SectorNumber) (info *SectorOnChainInf
 	} else {
 		return info, nil
 	}
+}
+
+// Loads info for a set of sectors to be proven.
+// If any of the sectors are declared faulty and not to be recovered, info for the first non-faulty sector is substituted instead.
+// If any of the sectors are declared recovered, they are returned from this method.
+func (sa Sectors) LoadForProof(provenSectors, expectedFaults bitfield.BitField) ([]*SectorOnChainInfo, error) {
+	nonFaults, err := bitfield.SubtractBitField(provenSectors, expectedFaults)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to diff bitfields: %w", err)
+	}
+
+	// Return empty if no non-faults
+	if empty, err := nonFaults.IsEmpty(); err != nil {
+		return nil, xerrors.Errorf("failed to check if bitfield was empty: %w", err)
+	} else if empty {
+		return nil, nil
+	}
+
+	// Select a non-faulty sector as a substitute for faulty ones.
+	goodSectorNo, err := nonFaults.First()
+	if err != nil {
+		return nil, xerrors.Errorf("failed to get first good sector: %w", err)
+	}
+
+	// Load sector infos
+	sectorInfos, err := sa.LoadWithFaultMask(provenSectors, expectedFaults, abi.SectorNumber(goodSectorNo))
+	if err != nil {
+		return nil, xerrors.Errorf("failed to load sector infos: %w", err)
+	}
+	return sectorInfos, nil
+}
+
+// Loads sector info for a sequence of sectors, substituting info for a stand-in sector for any that are faulty.
+func (sa Sectors) LoadWithFaultMask(sectors bitfield.BitField, faults bitfield.BitField, faultStandIn abi.SectorNumber) ([]*SectorOnChainInfo, error) {
+	standInInfo, err := sa.MustGet(faultStandIn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load stand-in sector %d: %v", faultStandIn, err)
+	}
+
+	// Expand faults into a map for quick lookups.
+	// The faults bitfield should already be a subset of the sectors bitfield.
+	sectorCount, err := sectors.Count()
+	if err != nil {
+		return nil, err
+	}
+	faultSet, err := faults.AllMap(sectorCount)
+	if err != nil {
+		return nil, fmt.Errorf("failed to expand faults: %w", err)
+	}
+
+	// Load the sector infos, masking out fault sectors with a good one.
+	sectorInfos := make([]*SectorOnChainInfo, 0, sectorCount)
+	err = sectors.ForEach(func(i uint64) error {
+		sector := standInInfo
+		faulty := faultSet[i]
+		if !faulty {
+			sectorOnChain, err := sa.MustGet(abi.SectorNumber(i))
+			if err != nil {
+				return xerrors.Errorf("failed to load sector %d: %w", i, err)
+			}
+			sector = sectorOnChain
+		}
+		sectorInfos = append(sectorInfos, sector)
+		return nil
+	})
+	return sectorInfos, err
+}
+
+func selectSectors(sectors []*SectorOnChainInfo, field bitfield.BitField) ([]*SectorOnChainInfo, error) {
+	toInclude, err := field.AllMap(uint64(len(sectors)))
+	if err != nil {
+		return nil, xerrors.Errorf("failed to expand bitfield when selecting sectors: %w", err)
+	}
+
+	included := make([]*SectorOnChainInfo, 0, len(toInclude))
+	for _, s := range sectors {
+		if !toInclude[uint64(s.SectorNumber)] {
+			continue
+		}
+		included = append(included, s)
+		delete(toInclude, uint64(s.SectorNumber))
+	}
+	if len(toInclude) > 0 {
+		return nil, xerrors.Errorf("failed to find %d expected sectors", len(toInclude))
+	}
+	return included, nil
 }
