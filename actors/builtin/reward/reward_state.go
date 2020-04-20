e3dbed76bb64354f7a9393dd0e98509adcc0efa5
witek <witek@enjin.io>
2020-04-20 18:26:36 +0000

    Minor fix for r152130. Put -fno-inline in f_Group.

--- actors/builtin/reward/reward_state.go
+++ actors/builtin/reward/reward_state.go
@@ -1,11 +1,10 @@
 package reward
 
 import (
-	abi "github.com/filecoin-project/go-state-types/abi"
-	big "github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/go-state-types/network"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/specs-actors/actors/util/smoothing"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
 )
 
 // A quantity of space * time (in byte-epochs) representing power committed to the network for some duration.
@@ -21,12 +20,14 @@ var InitialRewardPositionEstimate = big.MustFromString(InitialRewardPositionEsti
 // https://www.wolframalpha.com/input/?i=IntegerPart%5B%28Exp%5B-Log%5B2%5D+%2F+%286+*+%281+year+%2F+30+seconds%29%29%5D+-+1%29+*+10%5E18%5D
 var InitialRewardVelocityEstimate = abi.NewTokenAmount(-109897758509)
 
+// Changed since v0:
+// - ThisEpochRewardSmoothed is not a pointer
 type State struct {
 	// CumsumBaseline is a target CumsumRealized needs to reach for EffectiveNetworkTime to increase
 	// CumsumBaseline and CumsumRealized are expressed in byte-epochs.
 	CumsumBaseline Spacetime
 
-	// CumsumRealized is cumulative sum of network power capped by BalinePower(epoch)
+	// CumsumRealized is cumulative sum of network power capped by BaselinePower(epoch)
 	CumsumRealized Spacetime
 
 	// EffectiveNetworkTime is ceiling of real effective network time `theta` based on
@@ -43,7 +44,7 @@ type State struct {
 	// This value is recomputed every non-null epoch and used in the next non-null epoch.
 	ThisEpochReward abi.TokenAmount
 	// Smoothed ThisEpochReward
-	ThisEpochRewardSmoothed *smoothing.FilterEstimate
+	ThisEpochRewardSmoothed smoothing.FilterEstimate
 
 	// The baseline power the network is targeting at st.Epoch
 	ThisEpochBaselinePower abi.StoragePower
@@ -51,8 +52,16 @@ type State struct {
 	// Epoch tracks for which epoch the Reward was computed
 	Epoch abi.ChainEpoch
 
-	// TotalMined tracks the total FIL awared to block miners
-	TotalMined abi.TokenAmount
+	// TotalStoragePowerReward tracks the total FIL awarded to block miners
+	TotalStoragePowerReward abi.TokenAmount
+
+	// Simple and Baseline totals are constants used for computing rewards.
+	// They are on chain because of a historical fix resetting baseline value
+	// in a way that depended on the history leading immediately up to the
+	// migration fixing the value.  These values can be moved from state back
+	// into a code constant in a subsequent upgrade.
+	SimpleTotal   abi.TokenAmount
+	BaselineTotal abi.TokenAmount
 }
 
 func ConstructState(currRealizedPower abi.StoragePower) *State {
@@ -60,45 +69,47 @@ func ConstructState(currRealizedPower abi.StoragePower) *State {
 		CumsumBaseline:         big.Zero(),
 		CumsumRealized:         big.Zero(),
 		EffectiveNetworkTime:   0,
-		EffectiveBaselinePower: BaselineInitialValueV0,
+		EffectiveBaselinePower: BaselineInitialValue,
 
 		ThisEpochReward:        big.Zero(),
 		ThisEpochBaselinePower: InitBaselinePower(),
 		Epoch:                  -1,
 
 		ThisEpochRewardSmoothed: smoothing.NewEstimate(InitialRewardPositionEstimate, InitialRewardVelocityEstimate),
-		TotalMined:              big.Zero(),
+		TotalStoragePowerReward: big.Zero(),
+
+		SimpleTotal:   DefaultSimpleTotal,
+		BaselineTotal: DefaultBaselineTotal,
 	}
 
-	st.updateToNextEpochWithReward(currRealizedPower, network.Version0)
+	st.updateToNextEpochWithReward(currRealizedPower)
 
 	return st
 }
 
 // Takes in current realized power and updates internal state
 // Used for update of internal state during null rounds
-func (st *State) updateToNextEpoch(currRealizedPower abi.StoragePower, nv network.Version) {
+func (st *State) updateToNextEpoch(currRealizedPower abi.StoragePower) {
 	st.Epoch++
-	st.ThisEpochBaselinePower = BaselinePowerFromPrev(st.ThisEpochBaselinePower, nv)
+	st.ThisEpochBaselinePower = BaselinePowerFromPrev(st.ThisEpochBaselinePower)
 	cappedRealizedPower := big.Min(st.ThisEpochBaselinePower, currRealizedPower)
 	st.CumsumRealized = big.Add(st.CumsumRealized, cappedRealizedPower)
 
 	for st.CumsumRealized.GreaterThan(st.CumsumBaseline) {
 		st.EffectiveNetworkTime++
-		st.EffectiveBaselinePower = BaselinePowerFromPrev(st.EffectiveBaselinePower, nv)
+		st.EffectiveBaselinePower = BaselinePowerFromPrev(st.EffectiveBaselinePower)
 		st.CumsumBaseline = big.Add(st.CumsumBaseline, st.EffectiveBaselinePower)
 	}
 }
 
 // Takes in a current realized power for a reward epoch and computes
 // and updates reward state to track reward for the next epoch
-func (st *State) updateToNextEpochWithReward(currRealizedPower abi.StoragePower, nv network.Version) {
-	prevRewardTheta := computeRTheta(st.EffectiveNetworkTime, st.EffectiveBaselinePower, st.CumsumRealized, st.CumsumBaseline)
-	st.updateToNextEpoch(currRealizedPower, nv)
-	currRewardTheta := computeRTheta(st.EffectiveNetworkTime, st.EffectiveBaselinePower, st.CumsumRealized, st.CumsumBaseline)
+func (st *State) updateToNextEpochWithReward(currRealizedPower abi.StoragePower) {
+	prevRewardTheta := ComputeRTheta(st.EffectiveNetworkTime, st.EffectiveBaselinePower, st.CumsumRealized, st.CumsumBaseline)
+	st.updateToNextEpoch(currRealizedPower)
+	currRewardTheta := ComputeRTheta(st.EffectiveNetworkTime, st.EffectiveBaselinePower, st.CumsumRealized, st.CumsumBaseline)
 
-	st.ThisEpochReward = computeReward(st.Epoch, prevRewardTheta, currRewardTheta)
-
+	st.ThisEpochReward = computeReward(st.Epoch, prevRewardTheta, currRewardTheta, st.SimpleTotal, st.BaselineTotal)
 }
 
 func (st *State) updateSmoothedEstimates(delta abi.ChainEpoch) {
