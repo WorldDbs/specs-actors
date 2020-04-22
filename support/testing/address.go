cc0c13d3ef6d07aa66a3aa6019b3aed2898c0343
juan <juan@benet.ai>
2020-04-22 06:33:30 +0000

    Fixed: Translations

--- support/testing/address.go
+++ support/testing/address.go
@@ -1,26 +1,22 @@
 package testing
 
 import (
+	addr "github.com/filecoin-project/go-address"
+	"github.com/stretchr/testify/require"
 	"math/rand"
 	"testing"
-
-	addr "github.com/filecoin-project/go-address"
 )
 
 func NewIDAddr(t testing.TB, id uint64) addr.Address {
 	address, err := addr.NewIDAddress(id)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	return address
 }
 
 func NewSECP256K1Addr(t testing.TB, pubkey string) addr.Address {
 	// the pubkey of a secp256k1 address is hashed for consistent length.
 	address, err := addr.NewSecp256k1Address([]byte(pubkey))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	return address
 }
 
@@ -31,16 +27,12 @@ func NewBLSAddr(t testing.TB, seed int64) addr.Address {
 	r.Read(buf)
 
 	address, err := addr.NewBLSAddress(buf)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	return address
 }
 
 func NewActorAddr(t testing.TB, data string) addr.Address {
 	address, err := addr.NewActorAddress([]byte(data))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	return address
 }
