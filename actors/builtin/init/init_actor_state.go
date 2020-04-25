9a5d7be70d5cf97f7a930a6589c953e40cd3a151
igor <igor@soramitsu.co.jp>
2020-04-25 10:31:01 +0000

    crear celula

--- actors/builtin/init/init_actor_state.go
+++ actors/builtin/init/init_actor_state.go
@@ -2,14 +2,14 @@ package init
 
 import (
 	addr "github.com/filecoin-project/go-address"
-	abi "github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"
 	cid "github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	xerrors "golang.org/x/xerrors"
 
-	builtin "github.com/filecoin-project/specs-actors/actors/builtin"
-	autil "github.com/filecoin-project/specs-actors/actors/util"
-	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	autil "github.com/filecoin-project/specs-actors/v2/actors/util"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
 type State struct {
