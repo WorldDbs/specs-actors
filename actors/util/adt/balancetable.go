3edf1be32a239235955625107d4c4dd989c31333
boringland <boringland@protonmail.ch>
2020-05-13 23:51:39 +0000

    Revert #599 and #606

--- actors/util/adt/balancetable.go
+++ actors/util/adt/balancetable.go
@@ -2,11 +2,10 @@ package adt
 
 import (
 	addr "github.com/filecoin-project/go-address"
-	cid "github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
-
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
+	cid "github.com/ipfs/go-cid"
+	"golang.org/x/xerrors"
 )
 
 // A specialization of a map of addresses to (positive) token amounts.
