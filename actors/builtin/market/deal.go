4139535f673481775d363128501d85e269a5a7c2
witek <witek@enjin.io>
2020-05-15 17:02:38 +0000

    Configurable tracking log rotation (#3028)

--- actors/builtin/market/deal.go
+++ actors/builtin/market/deal.go
@@ -1,22 +1,16 @@
 package market
 
 import (
-	"bytes"
-
-	addr "github.com/filecoin-project/go-address"
-	abi "github.com/filecoin-project/go-state-types/abi"
-	big "github.com/filecoin-project/go-state-types/big"
-	acrypto "github.com/filecoin-project/go-state-types/crypto"
-	cid "github.com/ipfs/go-cid"
-	mh "github.com/multiformats/go-multihash"
+	market0 "github.com/filecoin-project/specs-actors/actors/builtin/market"
 )
 
-var PieceCIDPrefix = cid.Prefix{
-	Version:  1,
-	Codec:    cid.FilCommitmentUnsealed,
-	MhType:   mh.SHA2_256_TRUNC254_PADDED,
-	MhLength: 32,
-}
+//var PieceCIDPrefix = cid.Prefix{
+//	Version:  1,
+//	Codec:    cid.FilCommitmentUnsealed,
+//	MhType:   mh.SHA2_256_TRUNC254_PADDED,
+//	MhLength: 32,
+//}
+var PieceCIDPrefix = market0.PieceCIDPrefix
 
 // Note: Deal Collateral is only released and returned to clients and miners
 // when the storage deal stops counting towards power. In the current iteration,
@@ -26,60 +20,33 @@ var PieceCIDPrefix = cid.Prefix{
 // minimal deals that last for a long time.
 // Note: ClientCollateralPerEpoch may not be needed and removed pending future confirmation.
 // There will be a Minimum value for both client and provider deal collateral.
-type DealProposal struct {
-	PieceCID     cid.Cid `checked:"true"` // Checked in validateDeal, CommP
-	PieceSize    abi.PaddedPieceSize
-	VerifiedDeal bool
-	Client       addr.Address
-	Provider     addr.Address
-
-	// Label is an arbitrary client chosen label to apply to the deal
-	Label string
-
-	// Nominal start epoch. Deal payment is linear between StartEpoch and EndEpoch,
-	// with total amount StoragePricePerEpoch * (EndEpoch - StartEpoch).
-	// Storage deal must appear in a sealed (proven) sector no later than StartEpoch,
-	// otherwise it is invalid.
-	StartEpoch           abi.ChainEpoch
-	EndEpoch             abi.ChainEpoch
-	StoragePricePerEpoch abi.TokenAmount
-
-	ProviderCollateral abi.TokenAmount
-	ClientCollateral   abi.TokenAmount
-}
+//type DealProposal struct {
+//	PieceCID     cid.Cid `checked:"true"` // Checked in validateDeal, CommP
+//	PieceSize    abi.PaddedPieceSize
+//	VerifiedDeal bool
+//	Client       addr.Address
+//	Provider     addr.Address
+//
+//	// Label is an arbitrary client chosen label to apply to the deal
+//	// TODO: Limit the size of this: https://github.com/filecoin-project/specs-actors/issues/897
+//	Label string
+//
+//	// Nominal start epoch. Deal payment is linear between StartEpoch and EndEpoch,
+//	// with total amount StoragePricePerEpoch * (EndEpoch - StartEpoch).
+//	// Storage deal must appear in a sealed (proven) sector no later than StartEpoch,
+//	// otherwise it is invalid.
+//	StartEpoch           abi.ChainEpoch
+//	EndEpoch             abi.ChainEpoch
+//	StoragePricePerEpoch abi.TokenAmount
+//
+//	ProviderCollateral abi.TokenAmount
+//	ClientCollateral   abi.TokenAmount
+//}
+type DealProposal = market0.DealProposal
 
 // ClientDealProposal is a DealProposal signed by a client
-type ClientDealProposal struct {
-	Proposal        DealProposal
-	ClientSignature acrypto.Signature
-}
-
-func (p *DealProposal) Duration() abi.ChainEpoch {
-	return p.EndEpoch - p.StartEpoch
-}
-
-func (p *DealProposal) TotalStorageFee() abi.TokenAmount {
-	return big.Mul(p.StoragePricePerEpoch, big.NewInt(int64(p.Duration())))
-}
-
-func (p *DealProposal) ClientBalanceRequirement() abi.TokenAmount {
-	return big.Add(p.ClientCollateral, p.TotalStorageFee())
-}
-
-func (p *DealProposal) ProviderBalanceRequirement() abi.TokenAmount {
-	return p.ProviderCollateral
-}
-
-func (p *DealProposal) Cid() (cid.Cid, error) {
-	buf := new(bytes.Buffer)
-	if err := p.MarshalCBOR(buf); err != nil {
-		return cid.Undef, err
-	}
-	return abi.CidBuilder.Sum(buf.Bytes())
-}
-
-type DealState struct {
-	SectorStartEpoch abi.ChainEpoch // -1 if not yet included in proven sector
-	LastUpdatedEpoch abi.ChainEpoch // -1 if deal state never updated
-	SlashEpoch       abi.ChainEpoch // -1 if deal never slashed
-}
+//type ClientDealProposal struct {
+//	Proposal        DealProposal
+//	ClientSignature crypto.Signature
+//}
+type ClientDealProposal = market0.ClientDealProposal
