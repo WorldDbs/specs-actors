0e3934e52391e293599f2acf8b98dabdd5694942
alan.shaw <alan.shaw@protocol.ai>
2020-06-04 05:05:38 +0000

    Updated to UBL 2.3 CS01

--- actors/builtin/market/types.go
+++ actors/builtin/market/types.go
@@ -2,7 +2,7 @@ package market
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	. "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	. "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 
 	"github.com/ipfs/go-cid"
 )
@@ -15,7 +15,7 @@ type DealArray struct {
 
 // Interprets a store as balance table with root `r`.
 func AsDealProposalArray(s Store, r cid.Cid) (*DealArray, error) {
-	a, err := AsArray(s, r)
+	a, err := AsArray(s, r, ProposalsAmtBitwidth)
 	if err != nil {
 		return nil, err
 	}
@@ -38,8 +38,8 @@ func (t *DealArray) Set(k abi.DealID, value *DealProposal) error {
 	return t.Array.Set(uint64(k), value)
 }
 
-func (t *DealArray) Delete(key uint64) error {
-	return t.Array.Delete(key)
+func (t *DealArray) Delete(id abi.DealID) error {
+	return t.Array.Delete(uint64(id))
 }
 
 // A specialization of a array to deals.
@@ -56,7 +56,7 @@ type DealState struct {
 
 // Interprets a store as balance table with root `r`.
 func AsDealStateArray(s Store, r cid.Cid) (*DealMetaArray, error) {
-	dsa, err := AsArray(s, r)
+	dsa, err := AsArray(s, r, StatesAmtBitwidth)
 	if err != nil {
 		return nil, err
 	}
