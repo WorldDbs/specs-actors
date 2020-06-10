5380be5e68b7172b0962d577d77f2bd55eacff16
fjl <fjl@ethereum.org>
2020-06-10 06:58:07 +0000

    Make initialization more rational

--- actors/builtin/methods.go
+++ actors/builtin/methods.go
@@ -99,7 +99,8 @@ var MethodsMiner = struct {
 	ConfirmUpdateWorkerKey   abi.MethodNum
 	RepayDebt                abi.MethodNum
 	ChangeOwnerAddress       abi.MethodNum
-}{MethodConstructor, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23}
+	DisputeWindowedPoSt      abi.MethodNum
+}{MethodConstructor, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}
 
 var MethodsVerifiedRegistry = struct {
 	Constructor       abi.MethodNum
