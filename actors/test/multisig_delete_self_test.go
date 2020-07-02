82e3f6b31fb767330a5357440f7b4d0bee57b333
arajasek94 <arajasek94@gmail.com>
2020-07-02 16:02:38 +0000

    Be explicit about skipping

--- actors/test/multisig_delete_self_test.go
+++ actors/test/multisig_delete_self_test.go
@@ -8,69 +8,19 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/exitcode"
-	"github.com/filecoin-project/go-state-types/network"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	init_ "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
-	"github.com/filecoin-project/specs-actors/v2/support/ipld"
-	vm "github.com/filecoin-project/specs-actors/v2/support/vm"
+	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	init_ "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
+	"github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
+	"github.com/filecoin-project/specs-actors/v3/support/ipld"
+	vm "github.com/filecoin-project/specs-actors/v3/support/vm"
 )
 
-func TestV5MultisigDeleteSigner1Of2(t *testing.T) {
+func TestMultisigDeleteSelf2Of3RemovedIsProposer(t *testing.T) {
 	ctx := context.Background()
 	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
-	v, err := v.WithNetworkVersion(network.Version5)
-	require.NoError(t, err)
-	addrs := vm.CreateAccounts(ctx, t, v, 2, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
-
-	multisigParams := multisig.ConstructorParams{
-		Signers:               addrs,
-		NumApprovalsThreshold: 1,
-	}
-
-	paramBuf := new(bytes.Buffer)
-	err = multisigParams.MarshalCBOR(paramBuf)
-	require.NoError(t, err)
-
-	initParam := init_.ExecParams{
-		CodeCID:           builtin.MultisigActorCodeID,
-		ConstructorParams: paramBuf.Bytes(),
-	}
-	ret := vm.ApplyOk(t, v, addrs[0], builtin.InitActorAddr, big.Zero(), builtin.MethodsPower.CreateMiner, &initParam)
-	initRet := ret.(*init_.ExecReturn)
-	assert.NotNil(t, initRet)
-	multisigAddr := initRet.IDAddress
-
-	removeParams := multisig.RemoveSignerParams{
-		Signer:   addrs[0],
-		Decrease: false,
-	}
-
-	paramBuf = new(bytes.Buffer)
-	err = removeParams.MarshalCBOR(paramBuf)
-	require.NoError(t, err)
-
-	proposeRemoveSignerParams := multisig.ProposeParams{
-		To:     multisigAddr,
-		Value:  big.Zero(),
-		Method: builtin.MethodsMultisig.RemoveSigner,
-		Params: paramBuf.Bytes(),
-	}
-	// address 0 fails when trying to execute the transaction removing address 0
-	_, code := v.ApplyMessage(addrs[0], multisigAddr, big.Zero(), builtin.MethodsMultisig.Propose, &proposeRemoveSignerParams)
-	assert.Equal(t, exitcode.ErrIllegalState, code)
-	// address 1 succeeds when trying to execute the transaction removing address 0
-	vm.ApplyOk(t, v, addrs[1], multisigAddr, big.Zero(), builtin.MethodsMultisig.Propose, &proposeRemoveSignerParams)
-}
-
-func TestV5MultisigDeleteSelf2Of3RemovedIsProposer(t *testing.T) {
-	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
-	v, err := v.WithNetworkVersion(network.Version5)
-	require.NoError(t, err)
 	addrs := vm.CreateAccounts(ctx, t, v, 3, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
 
 	multisigParams := multisig.ConstructorParams{
@@ -79,7 +29,7 @@ func TestV5MultisigDeleteSelf2Of3RemovedIsProposer(t *testing.T) {
 	}
 
 	paramBuf := new(bytes.Buffer)
-	err = multisigParams.MarshalCBOR(paramBuf)
+	err := multisigParams.MarshalCBOR(paramBuf)
 	require.NoError(t, err)
 
 	initParam := init_.ExecParams{
@@ -119,11 +69,9 @@ func TestV5MultisigDeleteSelf2Of3RemovedIsProposer(t *testing.T) {
 
 }
 
-func TestV5MultisigDeleteSelf2Of3RemovedIsApprover(t *testing.T) {
+func TestMultisigDeleteSelf2Of3RemovedIsApprover(t *testing.T) {
 	ctx := context.Background()
 	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
-	v, err := v.WithNetworkVersion(network.Version5)
-	require.NoError(t, err)
 	addrs := vm.CreateAccounts(ctx, t, v, 3, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
 
 	multisigParams := multisig.ConstructorParams{
@@ -132,7 +80,7 @@ func TestV5MultisigDeleteSelf2Of3RemovedIsApprover(t *testing.T) {
 	}
 
 	paramBuf := new(bytes.Buffer)
-	err = multisigParams.MarshalCBOR(paramBuf)
+	err := multisigParams.MarshalCBOR(paramBuf)
 	require.NoError(t, err)
 
 	initParam := init_.ExecParams{
@@ -172,11 +120,9 @@ func TestV5MultisigDeleteSelf2Of3RemovedIsApprover(t *testing.T) {
 
 }
 
-func TestV5MultisigDeleteSelf2Of2(t *testing.T) {
+func TestMultisigDeleteSelf2Of2(t *testing.T) {
 	ctx := context.Background()
 	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
-	v, err := v.WithNetworkVersion(network.Version5)
-	require.NoError(t, err)
 	addrs := vm.CreateAccounts(ctx, t, v, 2, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
 
 	multisigParams := multisig.ConstructorParams{
@@ -185,7 +131,7 @@ func TestV5MultisigDeleteSelf2Of2(t *testing.T) {
 	}
 
 	paramBuf := new(bytes.Buffer)
-	err = multisigParams.MarshalCBOR(paramBuf)
+	err := multisigParams.MarshalCBOR(paramBuf)
 	require.NoError(t, err)
 
 	initParam := init_.ExecParams{
@@ -223,63 +169,9 @@ func TestV5MultisigDeleteSelf2Of2(t *testing.T) {
 	assert.Equal(t, exitcode.ErrNotFound, code)
 }
 
-func TestV5MultisigSwapsSelf1Of2(t *testing.T) {
+func TestMultisigSwapsSelf2Of3(t *testing.T) {
 	ctx := context.Background()
 	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
-	v, err := v.WithNetworkVersion(network.Version5)
-	require.NoError(t, err)
-	addrs := vm.CreateAccounts(ctx, t, v, 3, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
-	alice := addrs[0]
-	bob := addrs[1]
-	chuck := addrs[2]
-
-	multisigParams := multisig.ConstructorParams{
-		Signers:               []address.Address{alice, bob},
-		NumApprovalsThreshold: 1,
-	}
-
-	paramBuf := new(bytes.Buffer)
-	err = multisigParams.MarshalCBOR(paramBuf)
-	require.NoError(t, err)
-
-	initParam := init_.ExecParams{
-		CodeCID:           builtin.MultisigActorCodeID,
-		ConstructorParams: paramBuf.Bytes(),
-	}
-	ret := vm.ApplyOk(t, v, addrs[0], builtin.InitActorAddr, big.Zero(), builtin.MethodsPower.CreateMiner, &initParam)
-	initRet := ret.(*init_.ExecReturn)
-	assert.NotNil(t, initRet)
-	multisigAddr := initRet.IDAddress
-
-	swapParams := multisig.SwapSignerParams{
-		From: alice,
-		To:   chuck,
-	}
-
-	paramBuf = new(bytes.Buffer)
-	err = swapParams.MarshalCBOR(paramBuf)
-	require.NoError(t, err)
-
-	proposeSwapSignerParams := multisig.ProposeParams{
-		To:     multisigAddr,
-		Value:  big.Zero(),
-		Method: builtin.MethodsMultisig.SwapSigner,
-		Params: paramBuf.Bytes(),
-	}
-
-	// alice fails when trying to execute the transaction swapping alice for chuck
-	_, code := v.ApplyMessage(alice, multisigAddr, big.Zero(), builtin.MethodsMultisig.Propose, &proposeSwapSignerParams)
-	assert.Equal(t, exitcode.ErrIllegalState, code)
-	// bob succeeds when trying to execute the transaction swapping alice for chuck
-	vm.ApplyOk(t, v, bob, multisigAddr, big.Zero(), builtin.MethodsMultisig.Propose, &proposeSwapSignerParams)
-
-}
-
-func TestV5MultisigSwapsSelf2Of3(t *testing.T) {
-	ctx := context.Background()
-	v := vm.NewVMWithSingletons(ctx, t, ipld.NewBlockStoreInMemory())
-	v, err := v.WithNetworkVersion(network.Version5)
-	require.NoError(t, err)
 	addrs := vm.CreateAccounts(ctx, t, v, 4, big.Mul(big.NewInt(10_000), big.NewInt(1e18)), 93837778)
 	alice := addrs[0]
 	bob := addrs[1]
@@ -292,7 +184,7 @@ func TestV5MultisigSwapsSelf2Of3(t *testing.T) {
 	}
 
 	paramBuf := new(bytes.Buffer)
-	err = multisigParams.MarshalCBOR(paramBuf)
+	err := multisigParams.MarshalCBOR(paramBuf)
 	require.NoError(t, err)
 
 	initParam := init_.ExecParams{
@@ -440,5 +332,4 @@ func TestMultisigSwapsSelf1Of2(t *testing.T) {
 
 	// alice succeeds when trying to execute the transaction swapping alice for chuck
 	vm.ApplyOk(t, v, alice, multisigAddr, big.Zero(), builtin.MethodsMultisig.Propose, &proposeSwapSignerParams)
-
 }
