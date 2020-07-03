3116a3f4190cb28b651e2d9a52545a0793968ea7
fjl <fjl@ethereum.org>
2020-07-03 17:56:25 +0000

    use fileman for MiG scripts viewing

--- actors/builtin/cron/cron_state.go
+++ actors/builtin/cron/cron_state.go
@@ -4,7 +4,7 @@ import (
 	addr "github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
 )
 
 type State struct {
