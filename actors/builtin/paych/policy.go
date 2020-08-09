51206fe1b4d535e8364268ec743e341745460da0
igor <igor@soramitsu.co.jp>
2020-08-09 11:34:02 +0000

    Fix heading format in README.

--- actors/builtin/paych/policy.go
+++ actors/builtin/paych/policy.go
@@ -3,7 +3,7 @@ package paych
 import (
 	"math"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
 )
 
 // Maximum number of lanes in a channel.
