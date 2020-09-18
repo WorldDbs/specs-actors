19a2ce86dfbfad09286b0938ddd8063cb62a0541
ng8eke <ng8eke@163.com>
2020-09-18 12:35:22 +0000

    Merge "mdss: mdp3 driver support for solid fill"

---
+++ actors/migration/nv10/init.go
@@ -0,0 +1,41 @@
+package nv10
+
+import (
+	"context"
+
+	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
+	cid "github.com/ipfs/go-cid"
+	cbor "github.com/ipfs/go-ipld-cbor"
+
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
+)
+
+type initMigrator struct{}
+
+func (m initMigrator) migrateState(ctx context.Context, store cbor.IpldStore, in actorMigrationInput) (*actorMigrationResult, error) {
+	var inState init2.State
+	if err := store.Get(ctx, in.head, &inState); err != nil {
+		return nil, err
+	}
+
+	addressMapOut, err := migrateHAMTRaw(ctx, store, inState.AddressMap, builtin3.DefaultHamtBitwidth)
+	if err != nil {
+		return nil, err
+	}
+
+	outState := init3.State{
+		AddressMap:  addressMapOut,
+		NextID:      inState.NextID,
+		NetworkName: inState.NetworkName,
+	}
+	newHead, err := store.Put(ctx, &outState)
+	return &actorMigrationResult{
+		newCodeCID: m.migratedCodeCID(),
+		newHead:    newHead,
+	}, err
+}
+
+func (m initMigrator) migratedCodeCID() cid.Cid {
+	return builtin3.InitActorCodeID
+}
