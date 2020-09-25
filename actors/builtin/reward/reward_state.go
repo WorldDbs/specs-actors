f241121a963e1250d7be1103d2967d1117b8761f
ligi <ligi@ligi.de>
2020-09-25 11:13:08 +0000

    Update chengelogs for codimension version 2.2.0.

--- actors/builtin/reward/reward_state.go
+++ actors/builtin/reward/reward_state.go
@@ -4,7 +4,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
+	"github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
 )
 
 // A quantity of space * time (in byte-epochs) representing power committed to the network for some duration.
