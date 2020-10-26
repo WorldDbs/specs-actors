1cafa00799586de166e43c8a44772c5616525fa8
witek <witek@enjin.io>
2020-10-26 09:00:43 +0000

    Run service sequentially

--- actors/util/adt/array_test.go
+++ actors/util/adt/array_test.go
@@ -1,20 +1,20 @@
 package adt_test
 
 import (
-	"context"
 	"testing"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-	"github.com/filecoin-project/specs-actors/v2/support/mock"
+	"github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v3/support/mock"
 )
 
 func TestArrayNotFound(t *testing.T) {
-	rt := mock.NewBuilder(context.Background(), address.Undef).Build(t)
+	rt := mock.NewBuilder(address.Undef).Build(t)
 	store := adt.AsStore(rt)
-	arr := adt.MakeEmptyArray(store)
+	arr, err := adt.MakeEmptyArray(store, 3)
+	require.NoError(t, err)
 
 	found, err := arr.Get(7, nil)
 	require.NoError(t, err)
