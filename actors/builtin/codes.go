39e6ca44f6f877fc428d50b9772158158feb4973
davidad <davidad@alum.mit.edu>
2020-11-02 11:25:12 +0000

    Add new test to the one run for coveralls

--- actors/builtin/codes.go
+++ actors/builtin/codes.go
@@ -35,17 +35,17 @@ func init() {
 	builtinActors = make(map[cid.Cid]*actorInfo)
 
 	for id, info := range map[*cid.Cid]*actorInfo{ //nolint:nomaprange
-		&SystemActorCodeID:           {name: "fil/2/system"},
-		&InitActorCodeID:             {name: "fil/2/init"},
-		&CronActorCodeID:             {name: "fil/2/cron"},
-		&StoragePowerActorCodeID:     {name: "fil/2/storagepower"},
-		&StorageMinerActorCodeID:     {name: "fil/2/storageminer"},
-		&StorageMarketActorCodeID:    {name: "fil/2/storagemarket"},
-		&PaymentChannelActorCodeID:   {name: "fil/2/paymentchannel"},
-		&RewardActorCodeID:           {name: "fil/2/reward"},
-		&VerifiedRegistryActorCodeID: {name: "fil/2/verifiedregistry"},
-		&AccountActorCodeID:          {name: "fil/2/account", signer: true},
-		&MultisigActorCodeID:         {name: "fil/2/multisig", signer: true},
+		&SystemActorCodeID:           {name: "fil/3/system"},
+		&InitActorCodeID:             {name: "fil/3/init"},
+		&CronActorCodeID:             {name: "fil/3/cron"},
+		&StoragePowerActorCodeID:     {name: "fil/3/storagepower"},
+		&StorageMinerActorCodeID:     {name: "fil/3/storageminer"},
+		&StorageMarketActorCodeID:    {name: "fil/3/storagemarket"},
+		&PaymentChannelActorCodeID:   {name: "fil/3/paymentchannel"},
+		&RewardActorCodeID:           {name: "fil/3/reward"},
+		&VerifiedRegistryActorCodeID: {name: "fil/3/verifiedregistry"},
+		&AccountActorCodeID:          {name: "fil/3/account", signer: true},
+		&MultisigActorCodeID:         {name: "fil/3/multisig", signer: true},
 	} {
 		c, err := builder.Sum([]byte(info.name))
 		if err != nil {
