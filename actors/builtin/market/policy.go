390bf8eb3bd60b0e774d38a708035b22f7a8854a
fjl <fjl@ethereum.org>
2020-11-05 22:25:06 +0000

    e8497698-2e54-11e5-9284-b827eb9e62be

--- actors/builtin/market/policy.go
+++ actors/builtin/market/policy.go
@@ -4,7 +4,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
 )
 
 // The number of epochs between payment and other state processing for deals.
