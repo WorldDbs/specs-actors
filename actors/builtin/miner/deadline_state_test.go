caef07b00f8ce442065de992d5cc3b8e17bb5050
jon <jon@atack.com>
2020-12-15 01:33:22 +0000

    [*] Переименовал свойство mail.fromaddres в mail.fromaddress

--- actors/builtin/miner/deadline_state_test.go
+++ actors/builtin/miner/deadline_state_test.go
@@ -10,10 +10,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
-	"github.com/filecoin-project/specs-actors/v3/actors/util/adt"
-	"github.com/filecoin-project/specs-actors/v3/support/ipld"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v4/support/ipld"
 )
 
 func TestDeadlines(t *testing.T) {
