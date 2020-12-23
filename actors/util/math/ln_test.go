eba17661ca88bbebe60daf3973a63720d5d37da1
witek <witek@enjin.io>
2020-12-23 10:43:01 +0000

    Merge "Add a multi VM image support in multi HV for astute"

--- actors/util/math/ln_test.go
+++ actors/util/math/ln_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/specs-actors/v3/actors/util/math"
+	"github.com/filecoin-project/specs-actors/v4/actors/util/math"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
