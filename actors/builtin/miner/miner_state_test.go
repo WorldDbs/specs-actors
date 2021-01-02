bed015e6fa5c8633471bc04e0728dc2b6ddab43d
jon <jon@atack.com>
2021-01-02 01:25:04 +0000

    Implemented Try.apply(ThrowableFunction0)

--- actors/builtin/miner/miner_state_test.go
+++ actors/builtin/miner/miner_state_test.go
@@ -14,11 +14,11 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
-	"github.com/filecoin-project/specs-actors/v3/actors/util/adt"
-	"github.com/filecoin-project/specs-actors/v3/support/ipld"
-	tutils "github.com/filecoin-project/specs-actors/v3/support/testing"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v4/support/ipld"
+	tutils "github.com/filecoin-project/specs-actors/v4/support/testing"
 )
 
 func TestPrecommittedSectorsStore(t *testing.T) {
