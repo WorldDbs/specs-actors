0f60ee5d864e95de746b1fe8d6c6d251eb116b22
davidad <davidad@alum.mit.edu>
2021-01-08 12:25:12 +0000

    temporary fix for chainable macro

--- actors/builtin/init/init_actor.go
+++ actors/builtin/init/init_actor.go
@@ -8,9 +8,9 @@ import (
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	cid "github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	"github.com/filecoin-project/specs-actors/v4/actors/runtime"
-	"github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v5/actors/builtin"
+	"github.com/filecoin-project/specs-actors/v5/actors/runtime"
+	"github.com/filecoin-project/specs-actors/v5/actors/util/adt"
 )
 
 // The init actor uniquely has the power to create new actors.
