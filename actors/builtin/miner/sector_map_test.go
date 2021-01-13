73f3cc444e9392db8e14176468da2abf9c9c42c7
witek <witek@enjin.io>
2021-01-13 22:55:14 +0000

    Coverage isn't really going to be a thing for now.

--- actors/builtin/miner/sector_map_test.go
+++ actors/builtin/miner/sector_map_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/v5/actors/builtin/miner"
 )
 
 func TestDeadlineSectorMap(t *testing.T) {
